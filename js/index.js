f078d7e527448acbcf1489851e5a7d63a5813ca7
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-03-17 15:34:51 +0300

    start the timer

--- js/index.js
+++ js/index.js
@@ -7,3 +7,23 @@ const timerDisplay = document.getElementById('timer_display');
 const startButton = document.getElementById('start_button');
 const pauseButton = document.getElementById('pause_button');
 const resetButton = document.getElementById('reset_button');
+
+// Отоброжение времени 
+const updateDisplay = () => {
+    const time = Date.now() - startTime;
+    const minutes = Math.floor((time / 60000) %60).toString(). padStart(2,'0');
+    const seconds = Math.floor((time / 1000) %60).toString(). padStart(2,'0');
+    const milliseconds = Math.floor((time % 1000) / 10).toString(). padStart(2,'0');
+
+    timerDisplay.textContent = `${minutes}:${seconds}:${milliseconds}`;
+}
+
+// Запуск таймера 
+const startTimer = () => {
+    if (!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        intervalid = setInterval(updateDisplay,10);
+        isRunning = true;
+    }
+}
+
