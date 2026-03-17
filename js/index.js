91c72ba08eeb78c4c66b8d77d4ff2392ccf142d7
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-03-17 15:23:52 +0300

    make time display

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let startTime;
+let elapsedTime = 0;
+let isRunning = false;
+let intervalid;
+
+const timerDisplay = document.getElementById('timer_display');
+const startButton = document.getElementById('start_button');
+const pauseButton = document.getElementById('pause_button');
+const resetButton = document.getElementById('reset_button');
